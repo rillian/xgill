b65c4c78999d926c43e73080beb6df2dda4e4315
Brian Hackett <bhackett@stanford.edu>
2010-02-03 03:01:49 +0000

    oops missed a file
    git-svn-id: http://svn.sixgill.org/trunk@46 535b6189-6128-4a1a-9bee-cfdcb02e0d70

---
+++ backend/backend_block.h
@@ -0,0 +1,51 @@
+
+// Sixgill: Static assertion checker for C/C++ programs.
+// Copyright (C) 2009-2010  Stanford University
+// Author: Brian Hackett
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+// backend for writing out block data structures during the frontend parse.
+
+#include "backend.h"
+
+NAMESPACE_XGILL_BEGIN
+
+NAMESPACE_BEGIN(Backend)
+
+// determine which in a list of CSUs and/or blocks needs to be processed.
+// query_data is a compressed series of Names and/or BlockIds,
+// and result receives the subset of that list which needs to be processed.
+TAction* BlockQueryList(Transaction *t, TOperand *query_data,
+                        size_t var_result);
+
+// add the results of processing a set of CSUs and/or blocks. list is
+// a compressed series of CompositeCSUs, TAG_Uint32 followed by BlockCFGs,
+// EscapeEdgeSets, EscapeAccessSets, and CallEdgeSets.
+// these will be merged with any previous data.
+TAction* BlockWriteList(Transaction *t, TOperand *write_data);
+
+// determine whether an annotation CFG has already been processed.
+TAction* BlockQueryAnnot(Transaction *t, const char *db_name,
+                         const char *var_name, const char *annot_name,
+                         size_t var_result);
+
+// write out an annotation CFG stored in annot_data.
+TAction* BlockWriteAnnot(Transaction *t, TOperand *annot_data);
+
+NAMESPACE_END(Backend)
+
+NAMESPACE_XGILL_END
