32d9e974f02f73f693ccdf48f0244722e1c0a63c
Brian Hackett <bhackett@stanford.edu>
2013-01-09 16:17:20 +0000

    More gcc-4.7 fixes
    git-svn-id: http://svn.sixgill.org/trunk@160 535b6189-6128-4a1a-9bee-cfdcb02e0d70

--- gcc/missing.c
+++ gcc/missing.c
@@ -1,5 +1,6 @@
 
 #include <stdlib.h>
+#include "xgill.h"
 
 // symbols defined by cc1plus but not cc1. we need these stubs so that
 // we can use the plugin when compiling both C and CXX files. fortunately
@@ -7,8 +8,8 @@
 // symbols called instead.
 
 void lang_check_failed() { abort(); }
-void type_as_string() { abort(); }
-void decl_as_string() { abort(); }
-void cp_type_quals() { abort(); }
-void look_for_overrides_here() { abort(); }
-void get_template_info() { abort(); }
+const char * type_as_string(tree, int) { abort(); return NULL; }
+const char * decl_as_string(tree, int) { abort(); return NULL; }
+int cp_type_quals(const_tree) { abort(); return 0; }
+tree look_for_overrides_here(tree, tree) { abort(); return NULL; }
+tree get_template_info(const_tree) { abort(); return NULL; }
